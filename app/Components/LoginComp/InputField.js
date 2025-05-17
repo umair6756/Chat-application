2e5c5f78c5d5c49a669e8f82bead8a104494b7df
umair6756 <chumair6756@gmail.com>
2025-05-16 21:37:23 -0300

    Add login page

---
+++ app/Components/LoginComp/InputField.js
@@ -0,0 +1,32 @@
+export default function InputField({label, icon, placeholder, type}) {
+    return (
+       <>
+       <div className=" mx-auto bg-white px-9 py-3 h-auto w-[500px]">
+  <label
+    htmlFor="website-admin"
+    className="block mb-2 text-md font-bold text-gray-700 "
+  >
+    {label}
+  </label>
+  <div className="flex">
+    <span className="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border border-e-0 border-gray-300 ">
+      <div
+        className=""
+    
+      >
+        {icon}
+      </div>
+    </span>
+    <input
+      type={type}
+      id="website-admin"
+      className=" bg-white border border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm p-2.5  "
+      placeholder={placeholder}
+    />
+  </div>
+</div>
+
+       </>
+    )
+
+}
