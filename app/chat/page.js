e71cfba8cd54211601799b8bf98b0d63ce3fdba3
umair6756 <chumair6756@gmail.com>
2025-05-19 23:26:52 -0300

    Add chat Sidebar component

---
+++ app/chat/page.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Sidebar from '../Components/ChatPageComp/Sidebar'
+
+export default function page() {
+  return (
+    <div>
+        <Sidebar/>
+    </div>
+  )
+}
